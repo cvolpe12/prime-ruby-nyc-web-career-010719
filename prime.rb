41c1934e3fd553e2ebda5bf102421662f5fdfd29
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-02 20:58:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
 # Add  code here!
 def prime?(number)
-  
+  if number <= 1
+    false
+  elsif number == 2
+    true
+  else
+    (2..number-1).to_a.all? do |integer|
+      n % integer != 0
+    end
+  end
 end
